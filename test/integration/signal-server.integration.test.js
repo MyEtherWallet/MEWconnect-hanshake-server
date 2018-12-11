3a60e7d67ed8ddf1ba3f2129f3419d27e1673e25
Gage Siuniak <gsiuniak@gmail.com>
2018-12-10 16:54:56 -0800

    got socket tests working

--- test/integration/signal-server.integration.test.js
+++ test/integration/signal-server.integration.test.js
@@ -2,19 +2,23 @@
 
 // Imports //
 import SocketIO from 'socket.io'
+import SocketIOClient from 'socket.io-client'
 import Redis from 'ioredis'
 
 // Libs //
-import { redisConfig, serverConfig, socketConfig, signal, stages } from '@config'
+import { redisConfig, serverConfig, signal, stages } from '@config'
 import SignalServer from '@clients/signal-server'
 import RedisClient from '@clients/redis-client'
 
 describe('Signal Server', () => {
-  // Intialization //
-  describe('Init', () => {
+  // Instantiate SignalServer instance //
+  const signalServer = new SignalServer()
+
+  /**
+   * Initialization tests involving the instantiation of SignalServer
+   */
+  describe('init', () => {
     it('Should properly initialize', async () => {
-      // Create SignalServer instance //
-      const signalServer = new SignalServer()
       await signalServer.init()
 
       // HTTP Server //
@@ -32,6 +36,46 @@ describe('Signal Server', () => {
 
       // SocketIO //
       expect(signalServer.io instanceof SocketIO)
+    })
+  })
+
+  /**
+   * IO tests involving the socketIO connection of SignalServer.
+   * Good reference: https://medium.com/@tozwierz/testing-socket-io-with-jest-on-backend-node-js-f71f7ec7010f
+   */
+  describe('io', () => {
+    // Reused variables //
+    let socket
+    let serverAddress
+
+    // Initialize variables used in all tests //
+    beforeAll(async (done) => {
+      serverAddress = signalServer.server.address()
+      socket = await SocketIOClient.connect(`http://${serverAddress.address}:${serverAddress.port}`)
+      done()
+    })
+
+    // Connect to socketIO before each test //
+    beforeEach(async (done) => {
+      socket = await SocketIOClient.connect(`http://${serverAddress.address}:${serverAddress.port}`, {
+        'reconnection delay': 0,
+        'reopen delay': 0,
+        'force new connection': true,
+        transports: ['websocket']
+      })
+      socket.on('connect', () => {
+        done()
+      })
+    })
+
+    // Close socket connection after each test //
+    afterEach(async (done) => {
+      if (socket.connected) await socket.disconnect()
+      done()
+    })
+
+    it('Should be able to connect', () => {
+      expect(socket.connected).toBe(true)
     })
   })
 })
